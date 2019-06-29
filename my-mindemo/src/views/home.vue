3a1a806dae73d9caf94bf41acee875588470c4d6
liangliangMirror <jxliangweihan@gmail.com>
2019-06-29 18:04:11 +0800

    6-29

--- my-mindemo/src/views/home.vue
+++ my-mindemo/src/views/home.vue
@@ -1,14 +1,28 @@
 <template>
   <div>
     <heades/>
+    <div>{{$store.state.id}}</div>
   </div>
 </template>
 <script lang="ts">
 import Vue from "vue";
 import heades from "../components/heder.vue";
+import { mapActions, mapState, mapGetters } from "vuex";
 export default Vue.extend({
+  data() {
+    return {};
+  },
   components: {
     heades
+  },
+  mounted() {
+    console.log(this.$store.state.id);
+    console.log(this.$store.commit("SAVE_ID", 10));
+    console.log(this.$store.getters.add);
+    console.log(this.$store.dispatch("jiajia", 100));
+  },
+  updated() {
+    console.log(this.$store.state.id);
   }
 });
 </script>
